0a8abc89ae3b60d0fa8036cbe73c8f6ebf09d197
Mouli VJ <75278840+mouli0007@users.noreply.github.com>
2023-04-16 04:25:49 +0530

    Update Express_Middleware.js

--- Express_Middleware.js
+++ Express_Middleware.js
@@ -8,6 +8,11 @@ const { Logger } = require("./logger");
 // We can use app.use() to make a middleware function work instead of referencing it everywhere
 app.use("/about",Logger);
 
+// Using Multiple Middlewware function !
+
+app.use([Logger, authorize]);
+
+
 app.get("/", (req, res) => {
   res.send("Home");
 });
@@ -19,3 +24,22 @@ app.get("/about", (req, res) => {
 app.listen(5000, () => {
   console.log("Server is Listening to port 5000");
 });
+
+
+// Multiple Middleware
+// Function based on authorization !
+app.use([Logger, authorize]);
+
+const authorize = (req, res, next) => {
+  const { user } = req.query;
+
+  if (user === "mouli") {
+    req.user = { name: "mouli", id: 3 };
+    next();
+  } else {
+    res.status(401).send("Unauthorized");
+    return;
+  }
+};
+
+module.exports.authorize = authorize;
